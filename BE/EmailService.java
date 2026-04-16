f3fdc2dfd66eb86f29634d3d77cf584c1f81d6f7
Buihiep2006 <buingochiep987@gmail.com>
2026-04-16 21:53:26 +0700

    sdfsdf
    sdfsdf

--- BE/EmailService.java
+++
@@ -1,8 +0,0 @@
-package com.example.datvexemphim.service;
-
-import com.example.datvexemphim.entity.HoaDon;
-import com.example.datvexemphim.entity.KhachHang;
-
-public interface EmailService {
-    void sendBookingConfirmation(HoaDon hoaDon, String customerEmail, String customerName, String movieName, String showtime, String room, String seats, String total);
-}
